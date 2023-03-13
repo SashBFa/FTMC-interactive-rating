3497b08bf0ed88b026c8a4a2f5d4a3f0058ce05c
SashBFa <bastienfajs@gmail.com>
2023-03-13 10:53:55 +0100

    folder init

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export const App = () => {
+  return (
+    <main className="max-w-screen w-full h-screen bg-stone-900 flex justify-center items-center text-white">
+      <p>Hello there</p>
+    </main>
+  );
+};
